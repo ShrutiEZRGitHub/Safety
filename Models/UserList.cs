65598c8688cbe422a5373d24d9fbe3fc7830a53a
ShrutiEZRGitHub <shruti@ezrlabs.in>
2024-02-20 09:47:36 +0530

    20/02 Changes

--- Models/UserList.cs
+++ Models/UserList.cs
@@ -9,11 +9,11 @@ namespace Models
     {
         List<User> users = new List<User>()
         {
-            new User(){UserLevel="Level 1", FullName="Sameer",Depart="OSY-10",HOD="Yes",CNumber="+91 9145275485",Status="Active"},
-            new User(){UserLevel="Level 1", FullName="Jay",Depart="Department-1",HOD="No",CNumber="+91 9145275485",Status="InActive"},
+            new User(){UserLevel="Level 1", FullName="Sameer",Depart="North",HOD="Yes",CNumber="+91 9145275485",Status="Active"},
+            new User(){UserLevel="Level 5", FullName="Jay",Depart="Department-1",HOD="No",CNumber="+91 9145275485",Status="InActive"},
             new User(){UserLevel="Level 2", FullName="Viraj",Depart="OSY-10",HOD="Yes",CNumber="+91 9145275485",Status="Active"},
-            new User(){UserLevel="Level 1", FullName="Rahul",Depart="Department-3",HOD="No",CNumber="+91 9145275485",Status="InActive"},
-            new User(){UserLevel="Level 3", FullName="Amit",Depart="Department-2",HOD="Yes",CNumber="+91 9145275485",Status="Active"}
+            new User(){UserLevel="Level 4", FullName="Rahul",Depart="South",HOD="No",CNumber="+91 9145275485",Status="InActive"},
+            new User(){UserLevel="Level 3", FullName="Amit",Depart="West",HOD="Yes",CNumber="+91 9145275485",Status="Active"}
         };
         public async Task<List<User>> UserTable()
         {
